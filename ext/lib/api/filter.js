36ea332d68381a7bb46c772449c34c4b35a2ffa7
Sabrina del Poy <sabriidelpoy@gmail.com>
2020-10-09 21:20:39 -0300

    ajustes diseno de la home

--- ext/lib/api/filter.js
+++ ext/lib/api/filter.js
@@ -1,11 +1,48 @@
 const express = require('express')
 const debug = require('debug')
 const log = debug('democracyos:ext:api:filters')
-const api = require('ext/lib/db-api')
+const api = require('lib/backend/db-api')
+const apiExt = require('ext/lib/db-api')
 
 const app = module.exports = express()
 
-const limit = 3
+const limit = 10
+
+app.get(
+  '/topics/:byWhat/:page',
+  function(req, res, next) {
+    const options = {
+      limit,
+      skip: req.params.page * limit
+    }
+
+    switch (req.params.byWhat) {
+      case 'byClosed':
+        log('Filter by closed')
+        api.topic.findByClosed(options, (err, topics) => {
+          if(err) return next(err)
+          return res.json(topics)
+        })
+        break
+      case 'byPopular':
+        log('Filter by relevant')
+        api.topic.findByPopular(options, (err, topics) => {
+          if(err) return next(err)
+          return res.json(topics)
+        })
+        break
+      case 'byDate':
+        log('Filter by date')
+        api.topic.findByDate(options, (err, topics) => {
+          if(err) return next(err)
+          return res.json(topics)
+        })
+        break
+      default:
+        res.sendStatus(404)
+    }
+  }
+)
 
 app.get(
   '/:byWhat/:page',
@@ -18,21 +55,21 @@ app.get(
     switch (req.params.byWhat) {
       case 'byClosed':
         log('Filter by closed')
-        api.forum.findByClosed(options, (err, forums) => {
+        apiExt.forum.findByClosed(options, (err, forums) => {
           if(err) return next(err)
           return res.json(forums)
         })
         break
       case 'byPopular':
         log('Filter by relevant')
-        api.forum.findByPopular(options, (err, forums) => {
+        apiExt.forum.findByPopular(options, (err, forums) => {
           if(err) return next(err)
           return res.json(forums)
         })
         break
       case 'byDate':
         log('Filter by date')
-        api.forum.all(options, (err, forums) => {
+        apiExt.forum.all(options, (err, forums) => {
           if(err) return next(err)
           return res.json(forums)
         })
